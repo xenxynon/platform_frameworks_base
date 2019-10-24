466330a91a4eea6d0c17ec9d5423d26fa451e8ed
Dan Cartier <nospamdan1@gmail.com>
2019-10-24 10:14:49 -0400

    build: Don't check for fingerprint mismatch
    * This removes the warning that sidplays at boot which states:
      "There is an internal problem with your device.  Contact the manufacturer for details"
    
    Signed-off-by: Dan Cartier <nospamdan1@gmail.com>
    Change-Id: I5515f204bcdbe0a792f95aa1ab321fe4a8fd2696

--- core/java/android/os/Build.java
+++ core/java/android/os/Build.java
@@ -1371,7 +1371,7 @@ public class Build {
      */
     public static boolean isBuildConsistent() {
         // Don't care on eng builds.  Incremental build may trigger false negative.
-        if (IS_ENG) return true;
+        /*if (IS_ENG) return true;
 
         if (IS_TREBLE_ENABLED) {
             int result = VintfObject.verifyBuildAtBoot();
@@ -1382,6 +1382,7 @@ public class Build {
             }
 
             return result == 0;
+            return true;
         }
 
         final String system = SystemProperties.get("ro.system.build.fingerprint");
@@ -1404,7 +1405,7 @@ public class Build {
                         + " but vendor reported " + vendor);
                 return false;
             }
-        }
+        } */
 
         /* TODO: Figure out issue with checks failing
         if (!TextUtils.isEmpty(bootimage)) {
